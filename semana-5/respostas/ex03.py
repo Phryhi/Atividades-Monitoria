8796b987330dc7724f4ecc1c6b1d109551d8cb2f
Ana Julia Chiezi <112446399+Phryhi@users.noreply.github.com>
2026-03-17 11:54:01 -0300

    Create ex03.py

---
+++ semana-5/respostas/ex03.py
@@ -0,0 +1,24 @@
+a = float(input("nota a: "))
+b = float(input("nota b: "))
+c = float(input("nota c: "))
+
+if a < 3.0:
+    print("Reprovado")
+elif a <= 5.0:
+    print("Recuperação")
+else:
+    print("Aprovado")
+
+if b < 3.0:
+    print("Reprovado")
+elif b <= 5.0:
+    print("Recuperação")
+else:
+    print("Aprovado")
+
+if c < 3.0:
+    print("Reprovado")
+elif c <= 5.0:
+    print("Recuperação")
+else:
+    print("Aprovado")
